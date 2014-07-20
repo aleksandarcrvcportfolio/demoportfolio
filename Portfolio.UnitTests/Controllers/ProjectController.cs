965d2aef936bce784ef98a5c3b10526efe63ced9
aleksandarcrvcportfolio <aleksandar2cirovic@gmail.com>
2014-07-21 00:09:29 +0200

    Wrote some fucking pagination tests.

---
+++ Portfolio.UnitTests/Controllers/ProjectController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Portfolio.Domain.Abstract;
+using Portfolio.Domain.Entities;
+using System.Linq;
+using Portfolio.WebUI.Controllers;
+
+namespace Portfolio.UnitTests.Controllers
+{
+    [TestClass]
+    public class ProjectControllerTest
+    {
+        [TestMethod]
+        public void Can_Paginate()
+        {
+            // Arrange
+            var projRepository = new Mock<IProjectRepository>();
+            projRepository.Setup(x => x.Products).Returns(new Project[]
+            {
+                new Project {ProjectID = 1, Name = "P1"},
+                new Project {ProjectID = 2, Name = "P2"},
+                new Project {ProjectID = 3, Name = "P3"},
+                new Project {ProjectID = 4, Name = "P4"},
+                new Project {ProjectID = 5, Name = "P5"}
+            }.AsQueryable());
+
+            var projecController = new ProjectController(projRepository.Object);
+            projecController.pageSize = 3;
+            
+            // Act
+            var result = (IEnumerable<Project>)projecController.List(2).Model;
+            
+            // Assert
+            List<Project> prodArray = result.ToList();
+            Assert.IsTrue(prodArray.Count == 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+    }
+}
--- Portfolio.WebUI/Controllers/ProjectController.cs
+++ Portfolio.WebUI/Controllers/ProjectController.cs
@@ -10,15 +10,17 @@ namespace Portfolio.WebUI.Controllers
     public class ProjectController : Controller
     {
         private IProjectRepository _repository;
-
+        public int pageSize = 4;
         public ProjectController(IProjectRepository repository)
         {
             _repository = repository;
         }
         // GET: Project
-        public ActionResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(_repository.Products);
+            return View(_repository.Products.OrderBy(p => p.ProjectID)
+                .Skip((page - 1)* pageSize)
+                .Take(pageSize));
         }
     }
 }

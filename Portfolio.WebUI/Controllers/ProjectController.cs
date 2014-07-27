10c677a5056d94e8db86296ada336886192539c6
aleksandarcrvcportfolio <aleksandar2cirovic@gmail.com>
2014-07-27 19:25:27 +0200

    Remove unused namespace directives.

--- Portfolio.WebUI/Controllers/ProjectController.cs
+++ Portfolio.WebUI/Controllers/ProjectController.cs
@@ -1,8 +1,5 @@
 ﻿using Portfolio.Domain.Abstract;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Portfolio.WebUI.Controllers
--- Portfolio.WebUI/Infrastructure/NinjectControllerFactory.cs
+++ Portfolio.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -1,13 +1,8 @@
-﻿using Moq;
-using Ninject;
+﻿using Ninject;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Portfolio.Domain.Concrete;
-using Portfolio.Domain.Entities;
 using Portfolio.Domain.Abstract;
 
 namespace Portfolio.WebUI.Infrastructure
